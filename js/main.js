a861e55e53296295f1a205b9960a0b54d57c053a
Rodrigo Rio <rodrigo.rio@outlook.com.ar>
2024-09-11 11:56:38 -0300

    After

--- js/main.js
+++ js/main.js
@@ -1,75 +1,116 @@
-/**
- * CEIL, FLOOR Y ROUND
- */
-
-// console.log(Math.ceil(3.000001));
-
-// console.log(Math.floor(3.99999999999));
-
-// console.log(Math.round(3.5));
-
-/**
- * RANDOM
- */
-
-// const techo = parseInt(prompt("Ingrese hasta que número quiere un random"));
-//
-// const random = Math.round(Math.random() * techo);
-//
-// console.log(random);
-
-/**
- * EJEMPLO DE RANDOM
- */
-
-// class Producto {
-//     constructor(nombre) {
+// // Objetos
+// class Cliente {
+//     constructor(nombre, saldo) {
 //         this.nombre = nombre;
-//         this.precio = Math.round(Math.random() * 20 + 10);
+//         this.saldo = saldo;
+//     }
+//
+//     retirar(monto) {
+//         this.saldo = this.saldo - monto;
+//     }
+//
+//     depositar(monto) {
+//         this.saldo = this.saldo + monto;
 //     }
 // }
 //
-// const producto1 = new Producto("Tomates");
-// const producto2 = new Producto("Yogurt");
+// // Funciones
+// function obtenerCliente(mensajePrompt) {
+//     let nombreCliente = prompt(mensajePrompt);
+//     let cliente = clientes.find( (el) => el.nombre.toLowerCase() === nombreCliente.toLowerCase());
 //
-// console.log(producto1);
-// console.log(producto2);
+//     while(cliente === undefined) {
+//         alert("Nombre de cliente inválido, vuelva a ingresar otro");
+//         nombreCliente = prompt(mensajePrompt);
+//         cliente = clientes.find( (el) => el.nombre.toLowerCase() === nombreCliente.toLowerCase());
+//     }
+//
+//     return cliente;
+// }
+//
+// function transferirDinero() {
+//     // Obtener objetos de cliente del array
+//     const clienteTransfiere = obtenerCliente("Ingrese el nombre del cliente que va a realizar la transferencia");
+//     const clienteQueRecibe = obtenerCliente("Ingrese el nombre del cliente que va a recibir la transferencia");
+//
+//     let montoTransferencia = parseFloat(prompt("Ingrese el monto de la transferencia"));
+//
+//     while(montoTransferencia <= 0 || montoTransferencia > clienteTransfiere.saldo) {
+//         alert("VALOR INVÁLIDO");
+//         montoTransferencia = parseFloat(prompt("Ingrese el monto de la transferencia"));
+//     }
+//
+//     // Modificamos los saldos de los clientes
+//     clienteTransfiere.retirar(montoTransferencia);
+//     clienteQueRecibe.depositar(montoTransferencia);
+// }
+//
+// function consultarSaldo() {
+//     const cliente = obtenerCliente("Ingrese el nombre del cliente al que va a consultar el saldo");
+//
+//     alert("EL SALDO DEL CLIENTE ES: $" + cliente.saldo);
+// }
+//
+// // Inicio del programa
+// const clientes = [
+//     new Cliente("Pedro", 5000),
+//     new Cliente("Juan", 7500),
+//     new Cliente("Marcos", 2000),
+//     new Cliente("Gabriel", 3500),
+// ];
+//
+// let mensajePrompt = "Ingrese el número de la operación que quiere realizar. 1- Transferir dinero, 2- Consultar saldo, 0- Salir";
+// let opcion = parseInt(prompt(mensajePrompt));
+//
+// while(opcion !== 0) {
+//
+//     switch(opcion) {
+//         case 1:
+//             transferirDinero();
+//             break;
+//
+//         case 2:
+//             consultarSaldo();
+//             break;
+//     }
+//
+//     // Volvemos a pedir la opción al usuario
+//     opcion = parseInt(prompt(mensajePrompt));
+// }
 
 /**
- * OBJETO DATE
+ * EJERCICIOS ÓRDEN SUPERIOR
+ *
+ * 8)
  */
 
-// Instanciar fecha de hoy
-// const fechaHoy = new Date();
-//
-// console.log(fechaHoy);
+// Función
+function ordenarPorPropiedad(array, nombreDeLaPropiedad) {
 
-// Instanciar fecha de ayer
-// const fechaAyer = new Date(2024, 8, 9, 8, 30);
-//
-// console.log(fechaAyer);
+    array.sort( (a, b) => {
 
-// Obtener propiedades de una fecha
-// const fechaAyer = new Date(2024, 8, 9, 8, 30);
+        if(a[nombreDeLaPropiedad] > b[nombreDeLaPropiedad]) {
+            return 1;
+        } else if(a[nombreDeLaPropiedad] < b[nombreDeLaPropiedad]) {
+            return -1;
+        }
 
-// console.log(fechaAyer.getMinutes());
+        return 0;
+    });
 
-// Formatear fechas
-// console.log(fechaAyer.toLocaleDateString());
+    return array;
+}
 
-/**
- * DIFERENCIA ENTRE FECHAS
- */
+// Inicio del programa
 
-const fecha1 = new Date(2024, 7, 5, 10, 30); // Agosto
-const fecha2 = new Date(2024, 8, 5, 10, 30); // Septiembre
+const personas = [
+    { nombre: 'Juan', apellido: "Fernandez", edad: 30 },
+    { nombre: 'María', apellido: "Gomez", edad: 25 },
+    { nombre: 'Pedro', apellido: "Rodriguez", edad: 35 },
+    { nombre: 'Tomas', apellido: "Ramirez", edad: 20 },
+    { nombre: 'Matias', apellido: "Mendez", edad: 40 },
+];
 
-// const diferencia = fecha2 - fecha1;
-const diferencia = Math.abs(fecha1 - fecha2);
+const personasOrdenadasPorEdad = ordenarPorPropiedad(personas, 'edad');
 
-console.log(diferencia);
-
-// Calculo de dias
-// const milisegundosPorDia = 86400000;
-
-// console.log(diferencia / milisegundosPorDia);
+console.log(personasOrdenadasPorEdad);
