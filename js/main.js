61c6d4a5a49652be69c11a7004b130c346da2727
Rodrigo Rio <rodrigo.rio@outlook.com.ar>
2024-10-01 11:34:48 -0300

    Librerías

--- js/main.js
+++ js/main.js
@@ -1,275 +1,135 @@
-// const nombres = ["Juan", "Pedro", "Matias"];
-
-// console.log(nombres);
-
-// console.log(...nombres);
-// console.log(nombres[0], nombres[1], nombres[2]);
-
-/*
-    MAX & MIN
- */
-
-// const numeros = [1, 5, 60, 15, 20, 50, 75];
-
-// const maximo = Math.max(1, 5, 10, 15, 20);
-
-// const maximo = Math.max(numeros[0], numeros[1], numeros[2], numeros[3], numeros[4], numeros[5]);
-// const maximo = Math.max(...numeros);
-// const minimo = Math.min(...numeros);
-//
-// console.log(maximo);
-// console.log(minimo);
-
-/*
-    SPREAD EN ARRAYS
- */
-
-// const nombres = ["Juan", "Ramiro", "Gaston"];
-
-// const nuevosNombres = ["Juan", "Ramiro", "Gaston", "Pedro", "Matias"];
-// const nuevosNombres = [...nombres, "Pedro", "Matias"];
-//
-// console.log(nuevosNombres);
-
 /**
- * SPREAD EN OBJETOS
+ * SWEET ALERT
  */
 
-// const persona1 = {
-//     nombre: "Pedro",
-//     edad: 25,
-// }
+// Swal.fire({
+//     title: 'Eliminar producto',
+//     text: '¿Está seguro que desea eliminar el producto?',
+//     icon: 'warning',
+//     confirmButtonText: 'Si',
+//     showDenyButton: true,
+//     denyButtonText: "Cancelar",
+// }).then( (result) => {
 //
-// const persona2 = {
-//     ...persona1,
-// };
+//     if(result.isConfirmed) {
+//         // Eliminar el producto...
 //
-// persona2.nombre = "Juan";
-// console.log(persona2);
-
-// const persona1 = {
-//     nombre: "Pedro",
-//     edad: 25,
-//     profesion: "Profesor",
-// }
-//
-// const persona2 = {
-//     ...persona1,
-//     nombre: "Juan",
-//     edad: 30,
-// }
-//
-// console.log(persona2);
-
-/**
- * REST PARAMETERS
- */
-
-// function funcion1(...parametros) {
-//     console.log(parametros);
-// }
-//
-// funcion1("nombre", "nombre2", "nombre3", "nombre4", "nombre5")
-
-// function realizarOperacion(operacion, ...numeros) {
-//
-//     let resultado = 0;
-//
-//     if(operacion === "+") {
-//         resultado = numeros.reduce( (acc, el) => {
-//             return acc + el;
-//         }, 0);
-//     } else if(operacion === "-") {
-//         resultado = numeros.reduce( (acc, el) => {
-//             return acc - el;
-//         }, 0);
+//         Swal.fire({
+//             title: 'Producto eliminado',
+//             text: 'El producto fue eliminado',
+//             icon: 'success',
+//         });
+//     } else {
+//         Swal.fire({
+//             title: 'Producto no eliminado',
+//             text: 'Usted no eliminó el producto',
+//             icon: 'error',
+//         });
 //     }
-//
-//     console.log("El resultado es: " + resultado);
-// }
-//
-// realizarOperacion("+", 2, 5, 8, 10, 15);
-// realizarOperacion("-", 5, 10, 5);
+// });
 
 /**
- * EJEMPLO DE PREENTREGA 3
+ * TOASTIFY
  */
 
-// Objetos
-class Asiento {
-    constructor(id, seleccionado, comprado) {
-        this.id = id;
-        this.seleccionado = seleccionado;
-        this.comprado = comprado;
+// Toastify({
+//     text: "This is a toast",
+//     duration: 1000,
+//     // destination: "https://github.com/apvarun/toastify-js",
+//     newWindow: true,
+//     close: true,
+//     gravity: "top", // `top` or `bottom`
+//     position: "right", // `left`, `center` or `right`
+//     stopOnFocus: true, // Prevents dismissing of toast on hover
+//     style: {
+//         // background: "linear-gradient(to right, #00b09b, #96c93d)",
+//         background: "red",
+//     },
+//     onClick: function() {
+//         Toastify({
+//             text: "Nueva notificación",
+//             duration: 1000,
+//             newWindow: true,
+//             close: true,
+//             gravity: "top",
+//             position: "right",
+//             stopOnFocus: true,
+//             style: {
+//                 background: "green",
+//             },
+//         }).showToast();
+//     } // Callback after click
+// }).showToast();
 
-        this.divAsiento = this.generarHTML();
-    }
+/**
+ * LUXON
+ */
 
-    generarHTML() {
-        const divAsiento = document.createElement("div");
-        divAsiento.className = "asiento";
+const DateTime = luxon.DateTime;
 
-        // Si el asiento fue comprado
-        if(this.comprado) {
-            divAsiento.className += " comprado";
-        }
+// Instanciar fechas
+// const ayer = DateTime.local(2024, 9, 30, 10, 30);
+// const hoy = DateTime.now();
 
-        divAsiento.addEventListener("click", () => {
-            this.clickear();
-        });
+// Mostrar fecha
+// console.log(ayer.day);
+// console.log(ayer.zoneName);
 
-        return divAsiento;
-    }
+// Formatear fechas
+// console.log(ayer.toLocaleString());
+// console.log(ayer.setLocale('es').toLocaleString(DateTime.DATE_FULL));
 
-    comprar() {
-        this.comprado = true;
-        this.divAsiento.className += " comprado";
-    }
+// Operaciones
+// const nuevoAyer = ayer.plus({
+//     hours: 5,
+//     minutes: 30,
+// });
 
-    clickear() {
+// const nuevaFecha = ayer.plus({
+//     minutes: 190,
+// });
 
-        // Si el asiento fue comprado, imprimo mensaje y no hago nada más
-        if(this.comprado) {
-            alertaError("ESTE ASIENTO YA FUE COMPRADO");
-            return;
-        }
+// const nuevaFecha = ayer.minus({
+//     days: 5,
+// });
+//
+// console.log(ayer.toString());
+// console.log(nuevaFecha.toString());
 
-        if(this.seleccionado) {
+// const fecha = DateTime.local(2024, 9, 30, 11, 30);
+//
+// const nuevaFecha = fecha.set({
+//     hour: 13,
+//     minute: 45,
+// });
+//
+// console.log(nuevaFecha.toString());
 
-            // Deseleccionar asiento
-            this.seleccionado = false;
+/**
+ * CLASE DURATION
+ */
 
-            asientosSeleccionados = asientosSeleccionados.filter( (el) => {
-                return el.id !== this.id;
-            });
+// const Duration = luxon.Duration;
 
-            this.divAsiento.className = "asiento";
+// const hora = Duration.fromObject({
+//     hour: 10,
+//     minute: 30,
+// });
+//
+// const nuevaHora = hora.plus({
+//     hours: 5,
+//     minutes: 31,
+// });
 
-        } else {
+/**
+ * INTERVAL
+ */
 
-            // Seleccionar asiento
-            this.seleccionado = true;
+const Interval = luxon.Interval;
 
-            asientosSeleccionados.push(this);
+const fecha1 = DateTime.local(2024, 9, 25, 10);
+const fecha2 = DateTime.local(2024, 9, 28, 10);
 
-            this.divAsiento.className = "asiento seleccionado";
-        }
+const i = Interval.fromDateTimes(fecha1, fecha2);
 
-        calcularTotales();
-    }
-}
-
-
-// Funciones
-function alertaError(mensaje) {
-    divAlertaError.innerHTML = `<p>${mensaje}</p>`;
-}
-
-function alertaBueno(mensaje) {
-    divAlertaBueno.innerHTML = `<p>${mensaje}</p>`;
-}
-
-function calcularTotales() {
-
-    const total = asientosSeleccionados.length * 50;
-
-    pTotal.innerHTML = `$${total}`;
-}
-
-function asientoComprado(idAsiento) {
-    return asientosComprados.some( (el) => {
-        return el === idAsiento;
-    });
-}
-
-function obtenerAsientosComprados() {
-    return JSON.parse(localStorage.getItem("asientos_comprados")) || [];
-}
-
-function guardarAsientosComprados() {
-    localStorage.setItem("asientos_comprados", JSON.stringify(asientosComprados));
-}
-
-function comprarAsientos() {
-
-    if(asientosSeleccionados.length === 0) {
-        alertaError("No tiene asientos seleccionados");
-        return;
-    }
-
-    for(const asientoSeleccionado of asientosSeleccionados) {
-        asientosComprados.push(asientoSeleccionado.id);
-
-        // Compramos el asiento
-        asientoSeleccionado.comprar();
-    }
-
-    // Guardamos en local Storage los asientos comprados
-    guardarAsientosComprados();
-
-    // Limpiamos el array de asientos seleccionados
-    asientosSeleccionados = [];
-
-    // Calcular total
-    calcularTotales();
-
-    // Mostrar mensaje
-    alertaBueno("LOS ASIENTOS FUERON COMPRADOS");
-}
-
-function renderizarAsientos() {
-    divAsientos.innerHTML = "";
-    asientos = [];
-
-    // Recorro la fila de asientos
-    for(const filaAsientos of idsAsientos) {
-
-        const divFila = document.createElement("div");
-        divFila.className = "fila";
-
-        // Recorro los IDs de asientos
-        for(const idAsiento of filaAsientos) {
-
-            const asientoFueComprado = asientoComprado(idAsiento);
-
-            // Creo el objeto asiento y agrego el asiento a la fila
-            const asiento = new Asiento(
-                idAsiento,
-                false,
-                asientoFueComprado,
-            );
-
-            // Agrego el objeto a mi array de asientos
-            asientos.push(asiento);
-
-            divFila.append(asiento.divAsiento);
-        }
-
-        // Agrego la fila al div de asientos
-        divAsientos.append(divFila);
-    }
-}
-
-// Inicio del programa
-
-const divAsientos = document.getElementById("asientos");
-const botonComprar = document.getElementById("comprar");
-const pTotal = document.getElementById("total");
-const divAlertaError = document.getElementById("alertaError");
-const divAlertaBueno = document.getElementById("alertaBueno");
-
-const idsAsientos = [
-    ["A1", "A2", "A3", "A4", "A5", "A6"],
-    ["B1", "B2", "B3", "B4", "B5", "B6"],
-    ["C1", "C2", "C3", "C4", "C5", "C6"],
-    ["D1", "D2", "D3", "D4", "D5", "D6"],
-    ["E1", "E2", "E3", "E4", "E5", "E6"],
-];
-let asientos = [];
-let asientosSeleccionados = [];
-let asientosComprados = obtenerAsientosComprados();
-
-// Renderizar asientos
-renderizarAsientos();
-
-botonComprar.addEventListener("click", comprarAsientos);
+console.log(i.length('milliseconds'));
